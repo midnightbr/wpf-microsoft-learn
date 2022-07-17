df307acac343889d3f880d0af31cb79698a10642
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-07-16 23:47:58 -0300

    Salvando algumas alterações

--- PrimeiroWpf/PrimeiroWpf/obj/Debug/net5.0-windows/App.g.cs
+++ PrimeiroWpf/PrimeiroWpf/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace PrimeiroWpf {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace PrimeiroWpf {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
         public static void Main() {
             PrimeiroWpf.App app = new PrimeiroWpf.App();
             app.InitializeComponent();
